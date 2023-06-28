baeac298b17c3ae00653b6efcd7269c085d616c7
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-28 19:08:31 +0100

    create sidbar and library components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,8 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import { Box } from "./Box";
 import { SidebarItem } from "./SidebarItem";
+import { twMerge } from "tailwind-merge";
+import { Library } from "./Library";
 
 type SideBarPros = {
   children: React.ReactNode;
@@ -32,7 +34,16 @@ export const Sidebar = ({ children }: SideBarPros) => {
   );
   return (
     <div
-      className="
+      className={twMerge(
+        `
+        flex 
+        h-full
+        `
+        // player.activeId && "h-[calc(100%-80px)]"
+      )}
+    >
+      <div
+        className="
           hidden 
           md:flex 
           flex-col 
@@ -42,15 +53,19 @@ export const Sidebar = ({ children }: SideBarPros) => {
           w-[300px] 
           p-2
         "
-    >
-      <Box>
-        <div className="flex flex-col gap-y-4 px-5 py-4">
-          {routes.map((item) => (
-            <SidebarItem key={item.label} {...item} />
-          ))}
-        </div>
-      </Box>
-      <Box className="overflow-y-auto h-full">Song Library</Box>
+      >
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
