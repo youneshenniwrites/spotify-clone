580e04c4c3bfe813faf82968db0edd38b7fd6648
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-28 20:02:15 +0100

    add styling and props to custom button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import { Button } from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -92,6 +93,24 @@ export const Header = ({ children, className }: HeaderProps) => {
           >
             <BiSearch className="text-black" size={20} />
           </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                className="
+                    bg-transparent 
+                    text-neutral-300 
+                    font-medium
+                  "
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button className="bg-white px-6 py-2">Log in</Button>
+            </div>
+          </>
         </div>
       </div>
       {children}
