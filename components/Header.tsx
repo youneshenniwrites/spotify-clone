25e718ca6e14b00888871878b0d94a120ffcb008
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-28 19:41:07 +0100

    make header responsive with style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,100 @@
 "use client";
 
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
-type HeaderProps = {
+interface HeaderProps {
   children: React.ReactNode;
   className?: string;
-};
+}
 
 export const Header = ({ children, className }: HeaderProps) => {
   const router = useRouter();
 
   const handleLogout = () => {};
 
-  return <div>Header</div>;
+  return (
+    <div
+      className={twMerge(
+        `
+        h-fit 
+        bg-gradient-to-b 
+        from-emerald-800 
+        p-6
+        `,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="
+              rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
 };
