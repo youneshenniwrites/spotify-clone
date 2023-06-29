c8e8f2a3dc7286aad6bf18461abf1152360bb46c
youneshenniwrites <youneshenniwrites@gmail.com>
2023-06-29 13:18:03 +0100

    define a supabase provider component

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { Database } from "@/types/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+export const SupabaseProvider = ({ children }: SupabaseProviderProps) => {
+  const [supabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
